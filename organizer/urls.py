9532c8e2ef949290618a7860a4dcadb35690e151
Bhavesh Praveen <bhaveshpraveen10@gmail.com>
2018-03-29 17:50:53 +0530

    TeacherDetailView, StudentDetailView, CourseListView, CourseViewset

--- organizer/urls.py
+++ organizer/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path, re_path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 router = DefaultRouter()
 router.register(r'upload', views.PhotoUploadViewSet)
+router.register(r'course', views.CourseViewSet)
+
+
+non_router_urls = [
+    path('teacher/', views.TeacherDetailView.as_view()),
+    path('student/', views.StudentDetailView.as_view()),
+    path('course/list/', views.CourseListView.as_view()),
+]
+
+non_router_urls = format_suffix_patterns(non_router_urls)
 
 urlpatterns = [
-    re_path('^', include(router.urls))
+    re_path('^', include(non_router_urls)),
+    re_path('^', include(router.urls)),
 ]
 
--- organizer/views.py
+++ organizer/views.py
@@ -1,10 +1,21 @@
-from rest_framework import status, permissions
+from rest_framework import status, permissions, generics
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Photo, Course
-from .serializers import PhotoSerializer
+from .models import(
+    Photo,
+    Course, Teacher,
+    Student
+)
+from .serializers import(
+    TeacherSerializer,
+    StudentSerializer,
+    CourseListSerializer,
+    CourseSerializer,
+    PhotoSerializer,
+)
+from . import permissions as custom_permissions
 from .utils import generate_random_string, get_current_date, get_unique_identificaton
 
 
@@ -40,3 +51,41 @@ class PhotoUploadViewSet(ModelViewSet):
             print('Student', self.request.user.student)
 
             serializer.save(student=self.request.user.student, img=self.request.data.get('img'))
+
+
+
+class GetObjectMixin(generics.RetrieveAPIView):
+    def get_object(self):
+        obj = self.model.objects.get(user=self.request.user)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+
+#TODO: Permissions
+class TeacherDetailView(GetObjectMixin):
+    """Return the courses taken by the teacher along with the students in each class"""
+    model = Teacher
+    serializer_class = TeacherSerializer
+    permissions = (permissions.IsAuthenticated,)
+
+
+#TODO: Permissions
+class StudentDetailView(GetObjectMixin):
+    """Return the courses taken by student and the link to his pictures"""
+    model = Student
+    serializer_class = StudentSerializer
+    permissions = (permissions.IsAuthenticated,)
+
+
+#TODO: Permissions
+class CourseListView(generics.ListAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseListSerializer
+    permissions = (permissions.IsAuthenticated,)
+
+
+#TODO: Permissions
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = (permissions.IsAuthenticated, custom_permissions.IsTeacher)
