f25aa92946a682776eac684aa6afa0a8685019d0
Bhavesh Praveen <bhaveshpraveen10@gmail.com>
2018-03-29 20:52:00 +0530

    IsTeachersCourse and IsStudent custom permissions added

--- organizer/permissions.py
+++ organizer/permissions.py
@@ -2,11 +2,22 @@ from rest_framework import permissions
 
 
 class IsOwner(permissions.BasePermission):
-
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
 
 
 class IsTeacher(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.user.is_teacher
+        return request.user.is_teacher
+
+
+class IsStudent(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return not request.user.is_teacher
+
+
+class IsTeachersCourse(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.teacher == request.user
+    
+
