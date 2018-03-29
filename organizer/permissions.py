7e2b95813829508eccc6b164de737194ad4508f8
Bhavesh Praveen <bhaveshpraveen10@gmail.com>
2018-03-29 17:28:26 +0530

    Create IsTeacher custom permission

--- organizer/permissions.py
+++ organizer/permissions.py
@@ -5,3 +5,8 @@ class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+
+
+class IsTeacher(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user.is_teacher
